c290dc68a07ba3c7e65e7f332ecfe916349ace89
fengcaian <caianfeng@yeah.net>
2020-07-24 18:04:44 +0800

    g6-flow-editor-simple

---
+++ app/web/component/my-editor/detailPanel.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "detailPanel"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/web/component/my-editor/miniMap.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "miniMap"
+  }
+</script>
+
+<style scoped>
+
+</style>
